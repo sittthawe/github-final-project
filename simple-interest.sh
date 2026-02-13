88c63d57d745bc3d87da86dcbc3e03537a151600
sittthawe <128010151+sittthawe@users.noreply.github.com>
2026-02-13 12:08:54 +0700

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Simple Interest Calculator"
+
+echo "Enter Principal:"
+read principal
+
+echo "Enter Rate of Interest:"
+read rate
+
+echo "Enter Time Period:"
+read time
+
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+echo "Simple Interest = $simple_interest"
